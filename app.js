f4f8a5403cda6c4951a4d2c2da32527cafd5dbc7
Mohamed Safras <awsafras@gmail.com>
2022-06-13 09:01:42 +0530

    second commit

---
+++ app.js
@@ -0,0 +1,228 @@
+const video_continer = document.querySelector(".video_continer");
+const play_pause_btn = document.querySelector(".play_pause");
+const video = document.querySelector("video");
+const slider = document.querySelector("#volume-range");
+const volumeBtn = document.querySelector(".volume-btn");
+const currentTimeEl = document.querySelector("#current-time");
+const totalTimeEl = document.querySelector("#total-time"),
+  progress_bar = document.querySelector(".progress_bar"),
+  progress = document.querySelector(".progress");
+const soundProgress = document.querySelector(".progress-input");
+const video_lists = document.querySelector(".video_lists");
+const btn_next = document.querySelector("#btn_next");
+const screen_btn = document.querySelector(".screen_btn");
+const controlsBtn = document.querySelectorAll(".controls button");
+const settingsBtn = document.querySelector("[data-label=Settings]");
+
+const [play_or_pause, next_btn, mute_or_unmute] = controlsBtn;
+
+import video_data from "./data.js";
+
+let currentVideo = 0;
+
+const formatNumber = new Intl.NumberFormat("en-IN", {
+  minimumIntegerDigits: 2,
+});
+
+const formatTime = (time) => {
+  const hour = Math.floor(time / 3600);
+  const min = Math.floor(time / 60);
+  const sec = Math.floor(time % 60);
+
+  if (hour < 1) {
+    return `${formatNumber.format(min)}:${formatNumber.format(sec)}`;
+  } else {
+    return `${formatNumber.format(hour)}:${formatNumber.format(
+      min
+    )}:${formatNumber.format(sec)}`;
+  }
+};
+
+const play_pause = () => (video.paused ? video.play() : video.pause());
+
+play_pause_btn.addEventListener("click", play_pause);
+video.addEventListener("click", play_pause);
+video.addEventListener("play", () => {
+  video_continer.classList.remove("paused");
+  play_or_pause.dataset.label = "Pause (K)";
+});
+
+video.addEventListener("pause", () => {
+  video_continer.classList.add("paused");
+  play_or_pause.dataset.label = "Play (K)";
+  // console.log(controlsBtn.dataset.label);
+});
+
+video.addEventListener(
+  "loadeddata",
+  () => (totalTimeEl.textContent = formatTime(video.duration))
+);
+
+video.addEventListener("timeupdate", () => {
+  currentTimeEl.textContent = formatTime(video.currentTime);
+  progress.style.width = `${(video.currentTime / video.duration) * 100}%`;
+  video?.ended && nextVideo();
+});
+
+volumeBtn.addEventListener("click", toggleVolume);
+
+function toggleVolume() {
+  video.muted = !video.muted;
+  video.muted
+    ? (mute_or_unmute.dataset.label = "Unmute(m)")
+    : (mute_or_unmute.dataset.label = "Mute(m)");
+}
+
+slider.addEventListener(
+  "input",
+  (e) => {
+    video.volume = e.target.value;
+    video.muted = e.target.value === 0;
+    soundProgress.style.width = `${e.target.value * 100}%`;
+  },
+  false
+);
+
+video.addEventListener("volumechange", () => {
+  let type;
+  slider.value = video.volume;
+  if (video.muted || video.volume === 0) {
+    type = "mute";
+    slider.value = 0;
+  } else if (video.volume < 0.5) {
+    type = "low";
+  } else type = "high";
+  video_continer.dataset.volumeBtn = type;
+});
+
+progress_bar.addEventListener("click", changeCurrentTime);
+
+function changeCurrentTime(e) {
+  const progressWidth = progress_bar.clientWidth;
+  const currentWidth = e.offsetX;
+  video.currentTime = (currentWidth / progressWidth) * video.duration;
+}
+
+const changeVideo = (currentSong) => {
+  video.src = `./src/video/${video_data[currentSong].video}`;
+};
+
+const displayVideo = function () {
+  const items = video_data
+    .map(({ title, tumblrImage, id, youtubecannel }, index) => {
+      return `<li class="video_list" data-id=${id}>
+      <div class="video_list_left">
+        <div class="order">
+          <span>${index + 1}</span>
+        </div>
+        <div class="video_tumnail">
+          <img src="./src/image/${tumblrImage}" alt="" />
+        </div>
+      </div>
+      <div class="video_details">
+        <h4>${title.length > 50 ? title.substring(0, 100) + "..." : title}</h4>
+        <p class="category">${youtubecannel}</p>
+      </div>
+    </li>`;
+    })
+    .join("");
+  video_lists.insertAdjacentHTML("afterbegin", items);
+};
+displayVideo();
+
+function nextVideo() {
+  currentVideo === video_data.length - 1 ? (currentVideo = 0) : currentVideo++;
+  changeVideo(currentVideo);
+  activeList(currentVideo);
+  video.play();
+}
+
+btn_next.addEventListener("click", nextVideo);
+
+/**/
+window.addEventListener("load", () => {
+  changeVideo(currentVideo);
+  activeList(currentVideo);
+});
+
+const video_tumnail = document.querySelectorAll(".video_tumnail");
+
+function changeThumbnail(e, ishoverd) {
+  const id = +e.target.closest(".video_list").dataset.id - 1;
+  const [img] = e.target.children;
+  ishoverd
+    ? (img.src = `./src/image/${video_data[id]?.tumbnail}`)
+    : (img.src = `./src/image/${video_data[id]?.tumblrImage}`);
+}
+
+video_tumnail.forEach((item) => {
+  item.addEventListener("mouseenter", (e) => changeThumbnail(e, true));
+  item.addEventListener("mouseleave", (e) => changeThumbnail(e, false));
+});
+
+const video_list = document.querySelectorAll(".video_list");
+const order = document.querySelectorAll(".order span");
+
+function activeList(id) {
+  video_list.forEach((item) => {
+    item.classList.remove("active");
+  });
+  video_list[id].classList.add("active");
+  order.forEach((item, index) => {
+    index !== currentVideo
+      ? (item.innerHTML = index + 1)
+      : (item.innerHTML = `<svg
+    class="play"
+    height="100%"
+    version="1.1"
+    viewBox="0 0 36 36"
+    width="100%"
+  >
+    <use class="ytp-svg-shadow" xlink:href="#ytp-id-46"></use>
+    <path
+      class="ytp-svg-fill"
+      d="M 12,26 18.5,22 18.5,14 12,10 z M 18.5,22 25,18 25,18 18.5,14 z"
+      id="ytp-id-46"
+    ></path>
+  </svg>`);
+  });
+}
+
+video_lists.addEventListener("click", (e) => {
+  const target = e.target;
+  const closest = target.closest(".video_list").dataset.id - 1;
+  currentVideo = Number.parseInt(closest);
+  activeList(currentVideo);
+  changeVideo(currentVideo);
+  video.play();
+});
+
+document.addEventListener("fullscreenchange", () => {
+  video_continer.classList.toggle("fullscreen", document.fullscreenElement);
+});
+
+screen_btn.addEventListener("click", changeScreen);
+video.addEventListener("dblclick", changeScreen);
+
+function changeScreen() {
+  if (document.fullscreenElement == null) {
+    video_continer.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+}
+
+controlsBtn.forEach((item) => {
+  item.addEventListener("mouseenter", (e) => {
+    const target = e.target;
+
+    target.setAttribute("data-label", target.dataset.label);
+
+    target.dataset.label === "undefined"
+      ? target.classList.remove("show_label")
+      : target.classList.add("show_label");
+  });
+  item.addEventListener("mouseleave", (e) => {
+    e.target.classList.remove("show_label");
+  });
+});
---
+++ data.js
@@ -0,0 +1,73 @@
+export default [
+  {
+    id: 1,
+    title: "islamic WhatsApp status ❤️ Makkah ❤️betiful naat❤️",
+    video: "islamic WhatsApp status ❤️ Makkah ❤️betiful naat❤️.mp4",
+    tumblrImage: "islamic WhatsApp status ❤️ Makkah ❤️betiful naat❤️.webp",
+    tumbnail: "islamic WhatsApp status ❤️ Makkah ❤️betiful naat❤️.webp",
+    youtubecannel: "GHJdasdsa",
+  },
+  {
+    id: 2,
+    title:
+      "Oh Allah The Almighty ALLAH HOO ALLAH 😍 (Latest WhatsApp Islamic Status)",
+    video:
+      "Oh Allah The Almighty ALLAH HOO ALLAH 😍 (Latest WhatsApp Islamic Status).mkv",
+    tumbnail:
+      "Oh Allah The Almighty ALLAH HOO ALLAH 😍 (Latest WhatsApp Islamic Status).webp",
+    tumblrImage:
+      "Oh Allah The Almighty ALLAH HOO ALLAH 😍 (Latest WhatsApp Islamic Status).jpg",
+    youtubecannel: "ASD423",
+  },
+
+  {
+    id: 3,
+    title: "Bullet Song",
+    video: "Bullet Song.mp4",
+    tumblrImage: "bullet_song.jpg",
+    tumbnail: "bullet_song.webp",
+    youtubecannel: "Aditya Music Tamil",
+  },
+  {
+    id: 4,
+    title: "Get Out Of My Way _ KGF",
+    video: "Get Out Of My Way _ KGF .mp4",
+    tumblrImage: "Get Out Of My Way _ KGF.jpg",
+    tumbnail: "Get Out Of My Way _ KGF.webp",
+    youtubecannel: "00742fsd",
+  },
+  {
+    id: 5,
+    title: "Doctor - So Baby",
+    video: "Doctor - So Baby .mp4",
+    tumblrImage: "So-Baby.jpg",
+    tumbnail: "so_baby.webp",
+    youtubecannel: "XY32r2Z",
+  },
+  {
+    id: 6,
+    title: "Ramzan Mubarak Maula Ya Salli Wasalim - Natt Whatapp Status2022",
+    video: "Ramzan Mubarak Maula Ya Salli Wasalim.mp4",
+    tumblrImage:
+      "Ramzan Mubarak Maula Ya Salli Wasalim - Natt Whatapp Status2022.jpg",
+    tumbnail:
+      "Ramzan Mubarak Maula Ya Salli Wasalim - Natt Whatapp Status2022.webp",
+    youtubecannel: "ABC324sdf",
+  },
+  {
+    id: 7,
+    title: "Ayisha Abdul Basith - Muhammad Nabina",
+    video: "Ayisha Abdul Basith - Muhammad Nabina.mp4",
+    tumblrImage: "Ayisha Abdul Basith - Muhammad Nabina.jpg",
+    tumbnail: "Ayisha Abdul Basith - Muhammad Nabina.webp",
+    youtubecannel: "Ayesha Abdul Basit",
+  },
+  {
+    id: 8,
+    title: "Raataan Lambiyan",
+    video: "Raataan Lambiyan.mp4",
+    tumblrImage: "Raataan Lambiyan.jpg",
+    tumbnail: "Raataan Lambiyan.webp",
+    youtubecannel: "Sony Music",
+  },
+];
